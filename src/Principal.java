6921aa2bb09d77a7c38cc06826c1cd941366d79b
Mateus Melo <mateusdevmelo@gmail.com>
2024-04-19 21:41:05 -0300

    first commit

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+public class Principal {
+    public static void main(String[] args) {
+        Movie meuFilme = new Movie();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+        //meuFilme.somaDasAvaliacoes = 10;
+        //meuFilme.totalDeAvaliacoes = 1;
+        //System.out.println(meuFilme.pegaMedia());
+
+    }
+}
